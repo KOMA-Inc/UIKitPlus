d230bb85f44c4551d2fbaed2987c1bb906e4d2b3
gloomikon <gloomikon@gmail.com>
2025-01-22 10:02:59 +0100

    fix html attributed string

--- Sources/UIKitPlus/Extensions/Data+Extension.swift
+++ Sources/UIKitPlus/Extensions/Data+Extension.swift
@@ -4,14 +4,27 @@ public extension Data {
 
     var htmlAttributedString: NSAttributedString {
         get throws {
-            try NSAttributedString(
-                data: self,
-                options: [
-                    .documentType: NSAttributedString.DocumentType.html,
-                    .characterEncoding: String.Encoding.utf8.rawValue
-                ],
-                documentAttributes: nil
-            )
+            if #available(iOS 18.0, *) {
+                try NSAttributedString(
+                    data: self,
+                    options: [
+                        .documentType: NSAttributedString.DocumentType.html,
+                        .characterEncoding: String.Encoding.utf8.rawValue,
+                        .textKit1ListMarkerFormatDocumentOption: true
+                    ],
+                    documentAttributes: nil
+                )
+            } else {
+                try NSAttributedString(
+                    data: self,
+                    options: [
+                        .documentType: NSAttributedString.DocumentType.html,
+                        .characterEncoding: String.Encoding.utf8.rawValue
+                    ],
+                    documentAttributes: nil
+                )
+            }
+
         }
     }
 }
